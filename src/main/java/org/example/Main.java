bfb92388b228eb544ce3b91ed09c0c18554c5271
jagadeeshkumar <jagadeeshkumar@gigsky.com>
2024-04-29 11:43:08 +0530

    Refactored main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,37 @@ package org.example;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
+import org.example.interfaces.InputProcessor;
 import org.example.interfaces.Publisher;
+import org.example.interfaces.Validator;
 import org.example.services.AbstractProcessor;
 import org.example.services.BritishSpokenTimeProcessor;
 import org.example.services.CommandLineInput;
 import org.example.services.CommandLinePublisher;
 import org.example.services.validator.TwelveHourTimeValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static Logger LOG = LoggerFactory.getLogger("Main.class");
+
     public static void main(String[] args) {
 
         // set log level
-        Configurator.setRootLevel(Level.valueOf(System.getProperty("logLevel", "OFF")));
+        try {
+            Configurator.setRootLevel(Level.valueOf(System.getProperty("logLevel", "OFF")));
+        }
+        catch (IllegalArgumentException ex) {
+            LOG.error("Invalid value passed for logLevel property, defaulting the logLevel to OFF");
+            Configurator.setRootLevel(Level.valueOf("OFF"));
+        }
 
-        TwelveHourTimeValidator twelveHourTimeValidator = new TwelveHourTimeValidator();
+        LOG.info("Starting main method");
+
+        Validator<String> twelveHourTimeValidator = new TwelveHourTimeValidator();
         List<Publisher<String>> publishers = new ArrayList<>();
 
         Publisher<String> commandLinePublisher = new CommandLinePublisher();
@@ -27,7 +41,7 @@ public class Main {
         // Get processor, add validator and publishers
         AbstractProcessor<String, String> processor = new BritishSpokenTimeProcessor(twelveHourTimeValidator, publishers);
 
-        CommandLineInput commandLineInput = new CommandLineInput();
+        InputProcessor commandLineInput = new CommandLineInput();
         // start process
         commandLineInput.getInputAndProcess(processor);
     }
